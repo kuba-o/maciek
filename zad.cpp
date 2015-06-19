29659797bbd985a6f637a651232833b47d13b12c
kuba <kuba.ostrowicki@interia.pl>
2015-06-19 13:25:10 +0200

    first

---
+++ zad.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Czas
+{
+	public:
+    	void setLength( double len );
+    	double getLength( void );
+    	Line();  // This is the constructor
+ 
+	private:
+    	int godziny;
+    	int minuty;
+};
+
+
+int main(){
+	cout<<"sup";
+	return 0;
+}
