4a2853acd61cfef872104604210020ad7b8c6f98
kuba <kuba.ostrowicki@interia.pl>
2015-06-19 14:27:10 +0200

    dziala

--- zad.cpp
+++ zad.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 class Czas
@@ -8,8 +8,8 @@ class Czas
     	void podajCzas();
     	void wyswietl();
     	void zwiekszMinuty(int a);
-    	friend void porownajZegary(Czas zeg1, Czas zeg2);
-    	Czas();  // This is the constructor
+    	friend Czas porownajZegary(Czas zeg1, Czas zeg2);
+    	Czas();
     	Czas(int h, int m);
  
 	private:
@@ -30,6 +30,23 @@ void Czas::wyswietl(){
 	cout<<"Aktualny czas to "<<godziny<<":"<<minuty<<endl;
 }
 
+Czas porownajZegary(Czas zeg1, Czas zeg2){
+	int temp1=0;
+	int temp2=0;
+
+	temp1 += zeg1.godziny*60 + zeg1.minuty;
+	temp2 += zeg2.godziny*60 + zeg2.minuty;
+
+	if (temp1 > temp2){
+		cout<<"Zegar na którym jest późniejsza godzina to pierwszy podany argument."<<endl;
+		return zeg1;
+	}
+	else {
+		cout<<"Zegar na którym jest późniejsza godzina to drugi podany argument."<<endl;
+		return zeg2;
+	}
+}
+
 void Czas::zwiekszMinuty(int a){
 	int temp;
 	temp = a/60;
@@ -37,12 +54,15 @@ void Czas::zwiekszMinuty(int a){
 	minuty += a%60;
 }
 int main(){
+
 	Czas zegar1 = Czas();
 	zegar1.wyswietl();
-	Czas zegar2 = Czas(2, 10);
+	Czas zegar2 = Czas(4, 10);
 	zegar2.wyswietl();
 	zegar2.zwiekszMinuty(130);
 	zegar2.wyswietl();
+
+	porownajZegary(zegar1, zegar2).wyswietl();
 
 	return 0;
 }
